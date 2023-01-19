4b18436a9b1110a118f0c4c9a0c526042492d006
Harsh Soni <harshsoni@users.noreply.github.com>
2023-01-20 00:12:10 +0530

    Update .gitignore

--- blog/views.py
+++ blog/views.py
@@ -17,22 +17,19 @@ def blogPost(request, slug):
         return render(request, 'blog/blogPost.html',context)
 
 def postComment(request):
-        if request.method =="POST":
-                comment = request.POST.get("comment")
-                user = request.user
-                postSno = request.POST.get("postSno")
-                post = Post.objects.get(sno=postSno)
-                parentSno = request.POST.get("parentSno")
-                if parentSno == "":
-                        comment = BlogComment(comment=comment, user=user, post=post)
-                        comment.save()
-                        messages.success(request,'Your comment has been posted successfully')
-
-                else:
-                        parent = BlogComment.objects.get(sno=parentSno)
-                        comment = BlogComment(comment=comment, user=user, post=post, parent=parent)
-                        
-                        comment.save()
-                        messages.success(request,'Your reply has been posted successfully')
-        
-        return redirect(f"/blog/{post.slug}");
+    if request.method == "POST":
+        comment=request.POST.get('comment')
+        user=request.user
+        postSno =request.POST.get('postSno')
+        post= Post.objects.get(sno=postSno)
+        parentSno= request.POST.get('parentSno')
+        if parentSno=="":
+            comment=BlogComment(comment= comment, user=user, post=post)
+            comment.save()
+            messages.success(request, "Your comment has been posted successfully")
+        else:
+            parent= BlogComment.objects.get(sno=parentSno)
+            comment=BlogComment(comment= comment, user=user, post=post , parent=parent)
+            comment.save()
+            messages.success(request, "Your reply has been posted successfully")
+        return redirect(f"/blog/{post.slug}")
